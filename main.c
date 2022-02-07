dd8908288e83f2b7369abdd5b3b4bc7bd7a76f4c
wawerumacharia <97959618+wawerumacharia@users.noreply.github.com>
2022-02-07 16:50:45 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+COUNTY LIBRARY SYSTEM
+by jeff msh
+feb 2022
+mit licence
+c99 compiler
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu()
+       printf(countylibrary);
+        printf(countylibrary);
+         printf(countylibrary);
+          printf(countylibrary);
+
+
+
+int main()
+{
+    printf(countylibrary);
+    return 0;
+}
